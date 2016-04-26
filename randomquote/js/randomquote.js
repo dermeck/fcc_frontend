99cc89c0d4202e0fbf20ac7cce32726e53cd8275
Martin Runge <martinrunge@posteo.de>
2016-04-26 13:08:19 +0200

    random quote: implemented functionality for random quote api call

---
+++ randomquote/js/randomquote.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function () {
+    getRandomQuote();
+
+    // register event handler for button
+    $('#get-quote-btn').on('click', getRandomQuote);
+});
+
+
+function getRandomQuote() {
+    // call api
+    $.ajax({
+        headers: {
+            "X-Mashape-Key": "yUzR0sW4RamshuS8LpYNdzor19xhp1WKjTtjsndWCRWEDE2xLp",
+            Accept: "application/json",
+        },
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com',
+        success: function (response) {
+            // console.log(response);
+
+            json = JSON.parse(response);
+
+            var quote = json.quote;
+            var author = json.author;
+
+            // set content
+            $('#quote').text(quote);
+            $('#author').text(author);
+        }
+    })
+}
