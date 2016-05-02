777cb85c0f7dd8cab034f46085a2bbd9d05a9539
Martin Runge <martinrunge@posteo.de>
2016-05-02 16:00:35 +0200

    local weather: determine location

--- localweather/js/localweather.js
+++ localweather/js/localweather.js
@@ -1,3 +1,14 @@
+$(document).ready(function () {
+
+    $.getJSON('http://ipinfo.io', function(data){
+        // console.log(data);
+
+        var loc = data.loc.split(",");
+
+        getLocalWeather(loc[0], loc[1]);
+    })
+});
+
 /**
  * Created by martin on 02.05.16.
  */
@@ -7,6 +18,14 @@
 
 // get geo coordinates of user (modernizr?)
 
+/**
+ * Call open weather map API and passes response to updateUi
+ *
+ * @param latitude
+ * @param longitude
+ */
+function getLocalWeather(latitude, longitude) {
+
 // query weather api
 
-// set ui values
+// set ui values}}}
