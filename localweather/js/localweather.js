8e4510163c85353cb085c3a69337232abef295f5
Martin Runge <martinrunge@posteo.de>
2016-05-02 16:01:36 +0200

    local weather: call weather api and set city and condition

--- localweather/js/localweather.js
+++ localweather/js/localweather.js
@@ -10,13 +10,29 @@ $(document).ready(function () {
 });
 
 /**
- * Created by martin on 02.05.16.
+ * Populate UI with weather data
+ *
+ * @param data
  */
+function updateUi(data) {
+
+    // city
+    var city = data.name;
+    var country = data.sys.country;
+
+    var cityStr = city + ", " + country;
+    $('#location').text(cityStr);
+
+    // weather condition
+    var descr = data.weather[0].description;
+    console.log(descr);
+    $('#weather-condition').text(descr);
 
 
 
 
 // get geo coordinates of user (modernizr?)
+}
 
 /**
  * Call open weather map API and passes response to updateUi
@@ -25,7 +41,15 @@ $(document).ready(function () {
  * @param longitude
  */
 function getLocalWeather(latitude, longitude) {
+    var baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
+    var loc = "lat=" + latitude + "&lon=" + longitude;
+    var appId= "&APPID=" + "73683088432bf424ffcc937004bd296d";
 
-// query weather api
+    var url = baseUrl + loc + appId;
+    console.log(url);
 
-// set ui values}}}
+    $.getJSON(url, function (data) {
+        console.log(data);
+        updateUi(data);
+    })
+}
