465b8c0e30d2da4be50a36dd7d38eedb398a46b6
Martin Runge <martinrunge@posteo.de>
2016-05-05 14:15:03 +0200

    wikipedia search: refactoring

--- wikipediasearch/js/wikipediasearch.js
+++ wikipediasearch/js/wikipediasearch.js
@@ -1,11 +1,13 @@
-
 $(document).ready(function () {
 
+    registerStyleChangeEvents();
+});
+
+function registerStyleChangeEvents() {
+    // optical changes if search box is clicked
     $('#search').on('focus', function () {
         $(this).parent('div').addClass('search-active');
     })
-
     $('#search').on('blur', function () {
         $(this).parent('div').removeClass('search-active');
     })
-});
