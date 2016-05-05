7d100fc67e9ff2a43629563b85dae333705f7a56
Martin Runge <martinrunge@posteo.de>
2016-05-05 15:17:26 +0200

    wikipedia search: fetching search results from wikipedia / api call

--- wikipediasearch/js/wikipediasearch.js
+++ wikipediasearch/js/wikipediasearch.js
@@ -1,13 +1,58 @@
 $(document).ready(function () {
 
     registerStyleChangeEvents();
+
+    $('#search-icon').on('click', performSearch);
+
+
+    // performSearch();
 });
 
 function registerStyleChangeEvents() {
     // optical changes if search box is clicked
     $('#search').on('focus', function () {
         $(this).parent('div').addClass('search-active');
+
+        // TODO register event handler to trigger search when pressing ENTER
+
     })
     $('#search').on('blur', function () {
         $(this).parent('div').removeClass('search-active');
     })
+}
+
+function performSearch() {
+    // show loading indicator
+
+    // get search term
+    var searchTerm = $('#search').val();
+
+    // api reference https://www.mediawiki.org/wiki/API:Search
+
+    var url = "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchTerm + "&utf8=";
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        dataType: 'jsonp',
+        success: function (data) {
+            console.log(data);
+            displayResults(data);
+        }
+    });
+}
+
+function displayResults(data) {
+
+    var searchResultArray = data.query.search;
+
+    searchResultArray.forEach(function (value) {
+        console.log(value);
+        console.log(value.title);
+    });
+
+    console.log(data.query.search);
+
+    // TODO ... update ui
+}
